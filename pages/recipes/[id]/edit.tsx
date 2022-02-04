5239dce0dfe955e472f404c9d446346f1e752be4
christianwagstaff <=>
2022-02-03 17:50:32 -0800

    Recipe Edit Title updated

--- pages/recipes/[id]/edit.tsx
+++ pages/recipes/[id]/edit.tsx
@@ -3,6 +3,7 @@ import NewRecipeForm from '../../../components/NewRecipeForm'
 import { GetServerSideProps } from 'next'
 import { getRecipeData } from '../../../lib/recipes'
 import RecipeInterface from '../../../interfaces/recipe'
+import Head from 'next/head'
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (!params) {
@@ -16,6 +17,9 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 export default function NewRecipe({ recipe }: { recipe: RecipeInterface }) {
   return (
     <Layout>
+      <Head>
+        <title>Edit {recipe.name}</title>
+      </Head>
       <h1>Edit Recipe</h1>
       <NewRecipeForm edit={true} recipe={recipe} />
     </Layout>
